28c2a90627e186eb4785a6a9c85150c0ff89b0ba
SoponSufi <SoponSufi@DESKTOP-T52DVE1>
2025-04-16 02:02:39 +0530

    Add project files.

---
+++ CodeFirstASPCore6/Migrations/20250413201505_CodeFirstAddClass.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CodeFirstASPCore6.Migrations
+{
+    public partial class CodeFirstAddClass : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Standrad",
+                table: "Students",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Standrad",
+                table: "Students");
+        }
+    }
+}
---
+++ CodeFirstASPCore6/Models/Student.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CodeFirstASPCore6.Models
+{
+    public class Student
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Column("StudentName", TypeName ="nvarchar(50)")]
+        [Required]
+        public string Name { get; set; }
+
+        [Column("StudentGender", TypeName = "nvarchar(50)")]
+        [Required]
+        public string Gender { get; set; }
+        [Required]
+        public int Age { get; set; }
+        [Required]
+        public string Standrad { get; set; }
+
+
+
+    }
+}
